ee53d52a730a79e38d569a9b153c068f415667fc
sanjaysen <sanjay.sen@acuteinformatics.in>
2022-03-15 19:15:11 +0530

    email

--- backend/app.ts
+++ backend/app.ts
@@ -2,6 +2,7 @@ const fs = require("fs");
 import express from 'express';
 import cookieParse from 'cookie-parser';
 import bodyParser from 'body-parser';
+// import partnerOnboard from '';
 
 // import {error as errorMiddleware} from './middlewares/index';
 import {partnerEmailTemplate,transportConnection} from './config/index';
@@ -11,29 +12,38 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(cookieParse());
 
-app.get('/',async function (req, res) {
+//making route
+const partner = require('./routes/partner');
+
+app.use('/partner',partner);
+app.get('/sendemail',async function (req, res) {
     try{
-        const data:any =  fs.readFileSync(__dirname+'/temp.html');
+        const data:any =  fs.readFileSync('.\\backend\\templates\\partnerWelcome.html');
         
         let trans = await transportConnection(partnerEmailTemplate);
         let message = {
-            from: "sanjay.sen@acuteinformatics.in",
-            to: "milan.pithiya@acuteinformatics.in",
+            from: "sanjaysen558@gmail.com",
+            to: "sanjay.sen@acuteinformatics.in",
+            cc:"",
             subject: "Subject",
-            html: data
+            html: data,attachments:[{
+              filename:'Ratnaafin.png',
+              path:'.\\backend\\templates\\signature.png',  
+              cid:'signature'
+            }]
        }
        trans.sendMail(message, function(err:any, info:any) {
         if (err) {
+          res.send(err)
           console.log(err)
         } else {
+          res.send('Email successfully sent')
           console.log(info);
         }
     });
     }catch(e){
         console.log(e);
     }
-    
-    res.send('hello world')
 });
 //route
 // const auth = require('./routes/auth');
